1d81ae33c9c63d39d88fae0b49e9fe4ac9a6d882
Pedro <a18597@alunos.ipca.pt>
2019-12-03 17:53:33 +0000

    Construção da class Category.

---
+++ src/main/java/Category.java
@@ -0,0 +1,45 @@
+public class Category {
+
+    private String name;
+    private String description;
+    private String color;
+
+    public Category(){
+
+    }
+
+    public Category(String name){
+        this.name=name;
+    }
+
+    public Category(String name, String description, String color){
+        this.name = name;
+        this.description=description;
+        this.color = color;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+}
